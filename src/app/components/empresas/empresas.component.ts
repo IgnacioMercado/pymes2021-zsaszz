7a84656a5656137e2df0e1c1ea2f291e5e887682
IgnacioMercado <ignaciomercado00@gmail.com>
2022-06-22 13:19:13 -0300

    1 file modified, 1 added

--- src/app/components/empresas/empresas.component.ts
+++ src/app/components/empresas/empresas.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Empresas } from '../../models/Empresas';
+import { EmpresasService } from '../../services/Empresas.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ModalDialogService } from '../../services/modal-dialog.service';
+
+
 
 @Component({
   selector: 'app-empresas',
@@ -6,10 +12,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./empresas.component.css']
 })
 export class EmpresasComponent implements OnInit {
+  Items: Empresas[] = [];
+  Accion: string = 'L';
+  FormAlta: FormGroup = new FormGroup({
+    IdEmpresa: new FormControl(0),
+    RazonSocial: new FormControl('', [Validators.required]),
+    CantidadEmpleados: new FormControl('', [
+      Validators.pattern('[0-9]{1,6}'),
+      Validators.required,
+    ]),
+    FechaFundacion: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/](19|20)[0-9]{2}'
+      ),
+    ]),
+  });
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+
+  constructor(
+    private contactoService: ContactoService,
+    private modalDialogService: ModalDialogService
+  ) { }
+
+  ngOnInit() {}
+
+  
 
 }
